63b309b62ca77c1e69a136c5ab4fa63307783a45
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-12-03 00:56:22 +0900

    First sample: without handling mousemove / touchmove

--- Main.elm
+++ Main.elm
@@ -2,16 +2,37 @@ module Main where
 
 import Html exposing (..)
 import Html.Attributes exposing (style)
+import List
+import Maybe
 import Mouse
 import Signal exposing (Address)
 import Touch
 
+
 main : Signal Html
-main =
-  Signal.constant <|
-    div [ style [ ("margin-left", "2em"), ("margin-top", "2em"), ("font-size", "20pt") ] ]
-      [ div
-          [ style [ ("width", "20em"), ("height", "10em"), ("border", "1px solid black") ] ]
-          []
-      , p [] [text "Coordinates: "]
-      ]
+main = Signal.map view mergedCoord
+
+type alias Coord = Maybe (Int, Int)
+
+view : Coord -> Html
+view c =
+  div [ style [ ("margin-left", "2em"), ("margin-top", "2em"), ("font-size", "20pt") ] ]
+    [ div
+      [ style [ ("width", "20em"), ("height", "10em"), ("border", "1px solid black") ] ]
+      []
+    , p [] [text (toString c)]
+    ]
+
+
+mergedCoord : Signal Coord
+mergedCoord =
+  let mouse = Signal.map Just Mouse.position
+      touch =
+        Signal.map (Maybe.map toPair << List.head) Touch.touches
+  in
+  Signal.merge mouse touch
+
+
+toPair : Touch.Touch -> (Int, Int)
+toPair t = (t.x, t.y)
+
