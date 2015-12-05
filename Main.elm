8eaacd27c6c15af6d60c4235f21225974b75fceb
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-12-05 13:37:25 +0900

    :new: [NOT WORK] handle both touchmove/end and mousemove/end events
    but touchend event wasn't fired as I expected.
    
    Seems I have to detect that the touched point (finger) leaves the element (without leaving the display), by comparing the touched position and the target element position

--- Main.elm
+++ Main.elm
@@ -2,6 +2,8 @@ module Main where
 
 import Html exposing (..)
 import Html.Attributes exposing (style)
+import Html.Events exposing (..)
+import Json.Decode exposing (value)
 import List
 import Maybe
 import Mouse
@@ -10,29 +12,58 @@ import Touch
 
 
 main : Signal Html
-main = Signal.map view mergedCoord
+main = Signal.map view changingCoord
+
 
 type alias Coord = Maybe (Int, Int)
 
+
+type Action = In | Out
+
+
+actionsMailbox : Signal.Mailbox Action
+actionsMailbox = Signal.mailbox Out
+
+
 view : Coord -> Html
 view c =
-  div [ style [ ("margin-left", "2em"), ("margin-top", "2em"), ("font-size", "20pt") ] ]
-    [ div
-      [ style [ ("width", "20em"), ("height", "10em"), ("border", "1px solid black") ] ]
-      []
+  let outerStyle = [("margin-left", "2em"), ("margin-top", "2em"), ("font-size", "20pt")]
+      innerStyle = [("width", "20em"), ("height", "10em"), ("border", "1px solid black")]
+      emitIn  = onBoth ["mousemove",  "touchmove"] actionsMailbox.address In
+      emitOut = onBoth ["mouseleave", "touchend"]  actionsMailbox.address Out
+  in
+  div [style outerStyle]
+    [ div ([style innerStyle] ++ emitIn ++ emitOut) []
     , p [] [text (toString c)]
     ]
 
 
-mergedCoord : Signal Coord
-mergedCoord =
+onBoth : List String -> Signal.Address a -> a -> List Attribute
+onBoth events a x =
+  List.map (\event -> messageOn event a x) events
+
+
+-- NOTE: Copied from Html.Events. Why isn't it exported...?
+messageOn : String -> Signal.Address a -> a -> Attribute
+messageOn name addr msg =
+  on name value (\_ -> Signal.message addr msg)
+
+changingCoord : Signal Coord
+changingCoord =
   let mouse = Signal.map Just Mouse.position
       touch =
         Signal.map (Maybe.map toPair << List.head) Touch.touches
   in
-  Signal.merge mouse touch
+  Signal.map2 toggleInOut actionsMailbox.signal <| Signal.merge mouse touch
+
+
+toggleInOut : Action -> Coord -> Coord
+toggleInOut a c =
+  if a == In then
+    c
+  else
+    Nothing
 
 
 toPair : Touch.Touch -> (Int, Int)
 toPair t = (t.x, t.y)
-
